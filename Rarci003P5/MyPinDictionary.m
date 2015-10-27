0c856b15ceea62f7ed916ea7fc403b6e6195b528
rarciniegas <rarci003@fiu.edu>
2015-10-27 19:13:39 -0400

    Basic scaffold done

---
+++ Rarci003P5/MyPinDictionary.m
@@ -0,0 +1,13 @@
+//
+//  MyPinDictionary.m
+//  Rarci003P5
+//
+//  Created by roberto arciniegas on 10/27/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import "MyPinDictionary.h"
+
+@implementation MyPinDictionary
+
+@end
---
+++ Rarci003P5/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Rarci003P5
+//
+//  Created by roberto arciniegas on 10/27/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
