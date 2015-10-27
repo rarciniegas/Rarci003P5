6fc0cc7afbf4d9d859847e5559ff62d168290dcc
rarciniegas <rarci003@fiu.edu>
2015-10-27 18:22:11 -0400

    Initial Commit

---
+++ Rarci003P5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rarci003P5
+//
+//  Created by roberto arciniegas on 10/27/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
